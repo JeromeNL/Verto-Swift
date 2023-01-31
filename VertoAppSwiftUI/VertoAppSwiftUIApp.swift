f100508ccd612fad2163b251c2e9294588593620
JeromeNL <jw.kwetters@student.avans.nl>
2023-01-31 13:42:33 +0100

    Initial Commit

---
+++ VertoAppSwiftUI/VertoAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  VertoAppSwiftUIApp.swift
+//  VertoAppSwiftUI
+//
+//  Created by Joram Kwetters on 31/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct VertoAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
